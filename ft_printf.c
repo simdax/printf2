e50444f30184f0c5d2c4d00ec8aa5492a924e010
simdax <simoncornaz@gmail.com>
2017-12-15 18:23:23 +0100

    ok tests

--- ft_printf.c
+++ ft_printf.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+	/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
@@ -6,39 +6,56 @@
 /*   By: scornaz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 14:27:21 by scornaz           #+#    #+#             */
-/*   Updated: 2017/12/13 16:54:49 by simdax           ###   ########.fr       */
+/*   Updated: 2017/12/15 18:23:07 by scornaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
+typedef union   u_tt
+{
+  int		i;
+  unsigned int 	u;
+}		t_tt;
+
+
 void	dereference(char type, va_list argument, void *val)
 {
+  t_tt tt;
+  
   if ((ft_strany(type, "diDI")))
-    *(int*)val = va_arg(argument, int);
+    {
+      tt.i = va_arg(argument, int);
+      *(int*)val = tt.i;
+    }
   if ((ft_strany(type, "ouxOUX")))
-    *(unsigned int*)val = va_arg(argument, unsigned int);
+    {
+      tt.u = va_arg(argument, unsigned int);
+      *(unsigned int*)val = tt.u;
+    }
 }
 
-int	flags2print(va_list arg, t_flags flags)
+t_num	flags2print(va_list arg, t_flags flags)
 {
   t_num		a;
   void		*value;
 
-  dereference(flags.type, arg, value);
+  value = malloc(32);
+  dereference('d', arg, value);
   parse_value(value, flags.type, &a);
   a.left = flags.minus ? 0 : 1;
   a.sign = value > 0;
   a.padding = flags.width;
   a.precision = flags.precision;
   a.alternate = flags.hash;
-  a.type_padding = flags.zero ? '0' : ' ';
+  a.type_padding = flags.zero && !ft_strchr("DIOUXdioux", a.type) ? '0' : ' ';
   a.count = 0;
   a.precision = IF(a.precision - a.str_len);
   a.padding = IF(ABS(a.padding) - a.str_len + a.precision);
   print_arg(&a);
-  free(a.value);
-  return (a.count);
+//  free(a.value);
+  // free(value);
+  return (a);
 }
 
 
@@ -48,9 +65,8 @@ static int		count_percents(const char *str)
 
   count = 0;
   while (*str) {
-    if (*str == '%') {
+    if (*str == '%')
       count++;
-    }
     str++;
   }
   return (count);
@@ -60,23 +76,27 @@ int			ft_printf(const char* str, ...)
 {
   va_list	arg;
   int		nb_args;
-  t_flags	flags;
-  char	*cpy;
+  char		*cpy;
   int		count;
-
+  t_num		*nums;
+  t_flags flags;
+    
   count = 0;
   nb_args = count_percents(str);
+  nums = (t_num*)malloc(sizeof(t_num) * nb_args + 1);
   if (nb_args){
     va_start(arg, str);
     while (nb_args--) {
       cpy = ft_strchr(str, '%');
       count += cpy - str;
-      write(1, str, cpy - str);
-      flags = parse(cpy + 1);
-      count += flags2print(arg, flags);
-      str = cpy + flags.count + 1;
+      //write(1, str, cpy - str);
+	  flags = parse(cpy + 1);
+	  *nums = flags2print(arg, flags);
+	  count += nums->count;	  
+	  str = cpy + flags.count + 1;
+	  nums++;
     }
-    va_end(arg);
+	   va_end(arg);
     ft_putstr(str);
   }
   return (count);
